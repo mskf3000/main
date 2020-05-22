8e17a7e253d93aee1e6e5ac407793dbae1e49d19
mskf3000 <32472679+mskf3000@users.noreply.github.com>
2020-05-22 15:09:52 -0300

    Create amazonaws-prefixes.py
    https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html

---
+++ amazonaws-prefixes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html
+#
+import requests
+
+ip_ranges = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()['prefixes']
+amazon_ips = [item['ip_prefix'] for item in ip_ranges if item["service"] == "AMAZON"]
+ec2_ips = [item['ip_prefix'] for item in ip_ranges if item["service"] == "EC2"]
+
+amazon_ips_less_ec2=[]
+     
+for ip in amazon_ips:
+    if ip not in ec2_ips:
+        amazon_ips_less_ec2.append(ip)
+
+for ip in amazon_ips_less_ec2: print(str(ip))
